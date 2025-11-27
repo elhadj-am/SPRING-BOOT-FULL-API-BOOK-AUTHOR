fc8f5600b40f95f619bb29e987670a0fe4d0d13f
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2025-11-28 00:31:02 +0100

    testThatCreateAuthorSuccessfullyReturnsHttp201Created

--- src/test/java/Elhadj/SPRING_BOOT_DEVTIRO/database/controllers/AuthorControllersIntegrationTests.java
+++ src/test/java/Elhadj/SPRING_BOOT_DEVTIRO/database/controllers/AuthorControllersIntegrationTests.java
@@ -1,13 +1,46 @@
 package Elhadj.SPRING_BOOT_DEVTIRO.database.controllers;
 
+import Elhadj.SPRING_BOOT_DEVTIRO.database.TestDataUtil;
+import Elhadj.SPRING_BOOT_DEVTIRO.database.domain.entities.AuthorEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcResultMatchersDsl;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 public class AuthorControllersIntegrationTests {
 
+    private MockMvc mockMvc;
+    ObjectMapper objectMapper;
+
+    public AuthorControllersIntegrationTests(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testThatCreateAuthorSuccessfullyReturnsHttp201Created() throws Exception {
+        AuthorEntity authorEntity = TestDataUtil.createTestAuthorA();
+        authorEntity.setId(null);
+
+        String authroJson = objectMapper.writeValueAsString(authorEntity);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/authors")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(authroJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
 }
