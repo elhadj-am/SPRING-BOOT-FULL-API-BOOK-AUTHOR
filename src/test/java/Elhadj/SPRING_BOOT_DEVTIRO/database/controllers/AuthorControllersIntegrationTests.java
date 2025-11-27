9c9a6681ef2f321d8346ad7833609961746543ff
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2025-11-27 23:51:47 +0100

    Create AuthorControllersIntegrationTests.java

---
+++ src/test/java/Elhadj/SPRING_BOOT_DEVTIRO/database/controllers/AuthorControllersIntegrationTests.java
@@ -0,0 +1,13 @@
+package Elhadj.SPRING_BOOT_DEVTIRO.database.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+public class AuthorControllersIntegrationTests {
+
+}
