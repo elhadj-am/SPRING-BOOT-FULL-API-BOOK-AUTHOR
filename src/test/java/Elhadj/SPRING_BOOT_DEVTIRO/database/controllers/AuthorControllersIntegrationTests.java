8df16f694c2de7a899f0449b95f88f474a518931
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2025-11-29 20:07:08 +0100

    Update AuthorControllersIntegrationTests.java

--- src/test/java/Elhadj/SPRING_BOOT_DEVTIRO/database/controllers/AuthorControllersIntegrationTests.java
+++ src/test/java/Elhadj/SPRING_BOOT_DEVTIRO/database/controllers/AuthorControllersIntegrationTests.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -20,11 +21,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 public class AuthorControllersIntegrationTests {
 
+    @Autowired
     private MockMvc mockMvc;
     ObjectMapper objectMapper;
 
-    public AuthorControllersIntegrationTests(MockMvc mockMvc) {
-        this.mockMvc = mockMvc;
+    public AuthorControllersIntegrationTests() {
         this.objectMapper = new ObjectMapper();
     }
 
